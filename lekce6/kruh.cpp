da8010abcedfd9e5c2e715d207080196d9b6711d
JanMares248 <125661185+JanMares248@users.noreply.github.com>
2025-10-27 12:08:57 +0100

    Update kruh.cpp
    Jan Mareš cv6

--- lekce6/kruh.cpp
+++ lekce6/kruh.cpp
@@ -1,26 +1,57 @@
-#include <include>
+#include <vector>
+#include <iostream>
+#include <cmath>
 
-class Kruh
+using parametr = float;
+struct Kruh
 {
-   private:
+    Kruh() = default;
+    Kruh(parametr r)
+    {
+        radius = r;
+    }
 
-      float radius;
-}
+    void setRadius(parametr r)
+    {
+        radius = r;
+        
+    }
+    void vypisUdaje()
+    {
+        std::cout << "Radius je:" << radius << std::endl;
+        std::cout << "Plocha kruhu:" << spocitejPlochu() << std::endl;
+        std::cout << "Obvod kruhu:" << spocitejObvod() << std::endl;
+    }
+    parametr spocitejPlochu()
+    {
+        return  pi * pow(2, radius);
+        
+    }
+    parametr spocitejObvod()
+    {
+        return (parametr)2 * pi * radius;
+    }
+private:
+
+    parametr radius;
+    const parametr pi = (parametr)3.14;
+
+};
 
 int main()
 {
-   float prvniRadius = 2;
-   Kruh prvniKruh;
-   prvniKruh.setRadius( prvniRadius );
+    float prvniRadius = 2;
+    Kruh prvniKruh;
+    prvniKruh.setRadius(prvniRadius);
 
-   float druhyRaidus = 4;
-   Kruh druhyKruh( druhyRaidus );
-   std::cout << "Plocha kruhu:" << prvniKruh.spocitejPlochu() << std::stdl;
-   std::cout << "Obvod kruhu:" << prvniKruh.spocitejObvod() << std::stdl;
+    float druhyRaidus = 4;
+    Kruh druhyKruh(druhyRaidus);
+    std::cout << "Plocha kruhu:" << prvniKruh.spocitejPlochu() << std::endl;
+    std::cout << "Obvod kruhu:" << prvniKruh.spocitejObvod() << std::endl;
 
-   druhyKruh.vypisUdaje();
+    druhyKruh.vypisUdaje();
 
-   // BONUS: radius kruhu bude sablonovy parametr, a udela pole kruhu a pro kazdy kruh spocita vse
+    // BONUS: radius kruhu bude sablonovy parametr, a udela pole kruhu a pro kazdy kruh spocita vse
 
-   return 0;
+    return 0;
 }

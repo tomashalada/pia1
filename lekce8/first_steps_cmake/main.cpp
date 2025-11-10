ea8c009058c39b394d1dba7f5623059e2a851ea6
Tomáš Halada <halada.tms@gmail.com>
2025-11-10 10:44:38 +0100

    Added compilation

---
+++ lekce8/first_steps_cmake/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "math_functions.h"
+
+int main() {
+    std::cout << "5 + 3 = " << add(5, 3) << std::endl;
+    return 0;
+}
---
+++ lekce8/first_steps_make/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "math_functions.h"
+
+int main() {
+    int a;
+    std::cout << "5 + 3 = " << add(5, 3) << std::endl;
+    return 0;
+}
